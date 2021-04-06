d823b9c9fa24278d39485a0b69058bb5ea39c72e
Eli Turner <eyturner@umich.edu>
2021-04-05 18:21:18 -0700

    improving styling of dictionary feature

--- src/components/Sidebar.svelte
+++ src/components/Sidebar.svelte
@@ -21,23 +21,30 @@
         console.log(newWordOptions);
         wordOptions = newWordOptions;
     };
+
+    const setGridLine = (wordOption) => {
+        console.log("UPDATING GRID NOW! :D");
+    };
 </script>
 
 <div class="container">
     <button id="big-button" on:click={() => handleClick()}
         >Show me some words!</button
     >
+    {#if wordOptions}
+        <div class="word-container">
+            {#each wordOptions as wordOption}
+                <div
+                    class="word"
+                    on:click={(wordOption) => setGridLine(wordOption)}
+                >
+                    <p><strong>{wordOption.word}</strong></p>
+                    <p>{"\n" + wordOption.defs[0].slice(2)}</p>
+                </div>
+            {/each}
+        </div>
+    {/if}
 </div>
-{#if wordOptions}
-    <div class="word-container">
-        {#each wordOptions as wordOption}
-            <div class="word">
-                <p>{wordOption.word} \n</p>
-                <p>{wordOption.defs[0]}</p>
-            </div>
-        {/each}
-    </div>
-{/if}
 
 <style>
     .word-container {
@@ -45,7 +52,19 @@
         flex-direction: column;
         max-width: 30vw;
     }
+
+    .container {
+        max-height: 525px;
+        width: 30vw;
+        overflow: scroll;
+        border: 1px solid black;
+        padding: 5px;
+        border-radius: 5px;
+    }
+
     .word {
-        display: flex;
+        border: 1px solid grey;
+        margin: 5px 0px;
+        padding: 5px;
     }
 </style>
